c4b4f5e23c83543fa22425cd6ab76efe18520946
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-01-27 15:26:24 +0200

    Add Home Components

---
+++ client/src/components/Common/Loading.jsx
@@ -0,0 +1,11 @@
+export const Loading = () => {
+    return (
+        <div id="preloader">
+          <div className="jumper">
+            <div />
+            <div />
+            <div />
+          </div>
+        </div>
+    );
+}
---
+++ client/src/components/Home/RequestContact.jsx
@@ -0,0 +1,21 @@
+export const RequestContact = () => {
+    return (
+        <div className="request-form">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8">
+                <h4>Request a call back right now ?</h4>
+                <span>
+                  Contact us by filling out our contact form and we will connect with you as soon as possible.
+                </span>
+              </div>
+              <div className="col-md-4">
+                <a href="contact.html" className="border-button">
+                  Contact Us
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+    );
+}
