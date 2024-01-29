88c65b33371c40090b9193321b698fecc0b8feb2
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-01-29 15:14:49 +0200

    Delete unnecessary scripts

--- client/src/components/Home/Hero/Hero.jsx
+++ client/src/components/Home/Hero/Hero.jsx
@@ -22,42 +22,6 @@ export const Hero = () => {
                 </div>
               </div>
             </div>
-            <div className="item item-1">
-              <div className="img-fill">
-                <div className="text-content">
-                  <h6>Welcome to Rental Cars!</h6>
-                  <h4>
-                    Your ultimate destination
-                    <br /> 
-                    for car rentals
-                  </h4>
-                  <p>
-                    Your journey starts here at RentalCars – where convenience meets quality. Let the adventure begin!"
-                  </p>
-                  <a href="contact.html" className="filled-button">
-                    contact us
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="item item-1">
-              <div className="img-fill">
-                <div className="text-content">
-                  <h6>Welcome to Rental Cars!</h6>
-                  <h4>
-                    Your ultimate destination
-                    <br /> 
-                    for car rentals
-                  </h4>
-                  <p>
-                    Your journey starts here at RentalCars – where convenience meets quality. Let the adventure begin!"
-                  </p>
-                  <a href="contact.html" className="filled-button">
-                    contact us
-                  </a>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
     );
