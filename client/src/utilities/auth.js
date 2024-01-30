3f6bacccd58795814512a14289bb8ac35e3c268d
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-01-30 16:39:42 +0200

    Add authentication utilities

---
+++ client/src/utilities/auth.js
@@ -0,0 +1,18 @@
+const getUserInfo = () => {
+    return JSON.parse(sessionStorage.getItem('userData'));
+}
+
+const setUserInfo = (userData) => {
+    const userDataStr = JSON.stringify(userData);
+    sessionStorage.setItem('userData', userDataStr);
+}
+
+const clearUserInfo = () => {
+    sessionStorage.removeItem('userData');
+}
+
+export { 
+    getUserInfo ,
+    setUserInfo,
+    clearUserInfo
+}
