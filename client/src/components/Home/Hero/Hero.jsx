5e0aa8bc6a90fbf52341661fe1c2d02fb7969cf2
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-01-30 15:38:26 +0200

    Integrate Swiper JS

--- client/src/components/Home/Hero/Hero.jsx
+++ client/src/components/Home/Hero/Hero.jsx
@@ -1,28 +1,94 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+
+import { Link } from 'react-router-dom';
+import { Path } from '../../../utilities/Path'
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 import './Hero.css'
 
 export const Hero = () => {
     return (
-        <div className="main-banner header-text" id="top">
-          <div className="Modern-Slider">
-            <div className="item item-1">
-              <div className="img-fill">
-                <div className="text-content">
-                  <h6>Welcome to Rental Cars!</h6>
-                  <h4>
-                    Your ultimate destination
-                    <br /> 
-                    for car rentals
-                  </h4>
-                  <p>
-                    Your journey starts here at RentalCars – where convenience meets quality. Let the adventure begin!
-                  </p>
-                  <a href="contact.html" className="filled-button">
-                    contact us
-                  </a>
+      <>
+        <Swiper navigation={true} loop={true} modules={[Navigation]} className="mySwiper">
+          <SwiperSlide>
+            <div className="main-banner header-text" id="top">
+              <div className="Modern-Slider">
+                <div className="item item-1">
+                  <div className="img-fill">
+                    <div className="text-content">
+                      <h6>Welcome to Rental Cars!</h6>
+                      <h4>
+                        Your ultimate destination
+                        <br />
+                        for car rentals
+                      </h4>
+                      <p>
+                        Your journey starts here at RentalCars – where
+                        convenience meets quality. Let the adventure begin!
+                      </p>
+                      <Link to={Path.contact} className="filled-button">
+                        contact us
+                      </Link>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
+          </SwiperSlide>
+          <SwiperSlide>
+          <div className="main-banner header-text" id="top">
+              <div className="Modern-Slider">
+                <div className="item item-2">
+                  <div className="img-fill">
+                    <div className="text-content">
+                      <h6>Welcome to Rental Cars!</h6>
+                      <h4>
+                        Your ultimate destination
+                        <br />
+                        for car rentals
+                      </h4>
+                      <p>
+                        Your journey starts here at RentalCars – where
+                        convenience meets quality. Let the adventure begin!
+                      </p>
+                      <Link to={Path.contact} className="filled-button">
+                        contact us
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="main-banner header-text" id="top">
+              <div className="Modern-Slider">
+                <div className="item item-3">
+                  <div className="img-fill">
+                    <div className="text-content">
+                      <h6>Welcome to Rental Cars!</h6>
+                      <h4>
+                        Your ultimate destination
+                        <br />
+                        for car rentals
+                      </h4>
+                      <p>
+                        Your journey starts here at RentalCars – where
+                        convenience meets quality. Let the adventure begin!
+                      </p>
+                      <Link to={Path.contact} className="filled-button">
+                        contact us
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </>
     );
 }
