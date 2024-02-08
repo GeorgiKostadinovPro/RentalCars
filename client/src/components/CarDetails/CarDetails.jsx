758e64a5a1d00547ef380ae3848436b8a8d956b0
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-02-08 17:54:33 +0200

    Fix CSS paths

--- client/src/components/CarDetails/CarDetails.jsx
+++ client/src/components/CarDetails/CarDetails.jsx
@@ -1,8 +1,14 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Autoplay } from 'swiper/modules'
+
 import * as carService from '../../services/carService'
 import { RentForm } from "../RentForm/RentForm"
+
+import 'swiper/css'
+import 'swiper/css/navigation'
 
 import './CarDetails.css'
 
@@ -34,7 +40,8 @@ export const CarDetails = () => {
                   {carDetails.make} {carDetails.model}
                 </h1>
                 <p className="car-details-summary-p">
-                  For Rent <strong>|</strong> Price: ${carDetails.pricePerDay} / day
+                  For Rent <strong>|</strong> Price: ${carDetails.pricePerDay} /
+                  day
                 </p>
               </div>
             </div>
@@ -43,12 +50,37 @@ export const CarDetails = () => {
 
         <div className="car-details">
           <div className="left-container">
-            <h2>right side</h2>
+            <div className="car-gallery-slider">
+            {carDetails.gallery && carDetails.gallery.length > 0 ? (
+                <Swiper
+                  navigation={true}
+                  loop={true}
+                  autoplay={{ delay: 5000, disableOnInteraction: false }}
+                  modules={[Navigation, Autoplay]}
+                  className="mySwiper"
+                >
+                  {carDetails.gallery.map((image, i) => (
+                    <SwiperSlide key={i}>
+                      <div className="gallery-slide">
+                        <img src={image} alt="" />
+                      </div>
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
+              ) : (
+                <p>No images available</p>
+              )}
+            </div>
+
+            <div className="car-introduction">
+              <h2>{carDetails.make} {carDetails.model}</h2>
+              <p>Year of manufacture: {carDetails.year}</p>
+            </div>
           </div>
 
           <div className="right-container">
             <h2>Rent Now</h2>
-            
+
             <RentForm />
           </div>
         </div>
