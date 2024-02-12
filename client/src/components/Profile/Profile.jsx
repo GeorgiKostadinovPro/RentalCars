d9128b69abf516c02821a33d42d2a5a8a98d79ac
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-02-12 16:56:41 +0200

    Add Profile page component

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,10 +1,8 @@
-import '../Common/Banner.css'
-
 import './Profile.css'
 
 export const Profile = () => {
     return (
-        <>
+      <>
         <div className="page-heading header-text">
           <div className="container">
             <div className="row">
@@ -15,6 +13,45 @@ export const Profile = () => {
             </div>
           </div>
         </div>
-        </>
-    )
+
+        <div className="profile-container">
+          <div className="side-menu">
+            <img
+              src="https://www.shutterstock.com/image-vector/default-avatar-profile-icon-social-600nw-1677509740.jpg"
+              alt=""
+            />
+
+            <ul>
+              <li><a href="">Home</a></li>
+              <li><a href="">Created Cars</a></li>
+              <li><a href="">Rented Cars</a></li>
+              <li><a href="">Logout</a></li>
+            </ul>
+
+          </div>
+          <div className="profile-content">
+            <h2>Your Personal Info</h2>
+
+            <div className="text">
+              <p>Username</p>
+              <p>Peter</p>
+            </div>
+
+            <hr />
+
+            <div className="text">
+              <p>Email</p>
+              <p>peter@abv.bg</p>
+            </div>
+
+            <hr />
+
+            <div className="text">
+              <p>Phone</p>
+              <p>+359879454529</p>
+            </div>
+          </div>
+        </div>
+      </>
+    );
 }
