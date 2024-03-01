9e7690fe2f773563936949257fddf810203e3487
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-03-01 16:43:06 +0200

    Finish Single Comment Styles

--- client/src/components/Comments/Comments.jsx
+++ client/src/components/Comments/Comments.jsx
@@ -1,24 +1,27 @@
 import { useEffect } from 'react'
 
 import * as commentService from '../../services/commentService'
+import { Comment } from './Comment/Comment'
+import { Pagination } from '../Pagination/Pagination'
 
 import './Comments.css'
 
 export const Comments = () => {
-    return (
-      <div className="comments-container">
-        <h3>Comments (14)</h3>
-        <hr />
-        <div className="post-tags">
-          {/* <ul>
-            {postDetails.tags &&
-              postDetails.tags.map((tag, i) => (
-                <li key={i}>
-                  <a href="#">{tag}</a>
-                </li>
-              ))}
-          </ul> */}
-        </div>
-      </div>
-    );
+
+
+  return (
+    <div className="comments-container">
+      <h3>Comments (14)</h3>
+      <hr />
+
+      <Comment />
+      <Comment />
+      <Comment />
+
+      <br />
+      <br />
+
+      <Pagination currPage={1} totalPages={2} handlePageChange={() => {}} />
+    </div>
+  );
 }
