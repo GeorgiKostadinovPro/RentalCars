4d39f3e1cb3e99d112b4bc9480293dfa20e34aef
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-03-08 20:58:08 +0200

    Finish Error Boundary Implementation

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,26 +1,26 @@
 import { Facts } from "./Facts"
 import { Info } from "./Info"
-//import { Team } from "./Team/Team"
+import { Team } from "./Team/Team"
 
 export const About = () => {
-    return (
-      <>
-        <div className="page-heading header-text">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12">
-                <h1>About Us</h1>
-                <span>We have an excellent team and ambitious goals</span>
-              </div>
+  return (
+    <>
+      <div className="page-heading header-text">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1>About Us</h1>
+              <span>We have an excellent team and ambitious goals</span>
             </div>
           </div>
         </div>
+      </div>
 
-        <Info />
-        
-        <Facts />
+      <Info />
 
-        <Team />
-      </>
-    );
+      <Facts />
+
+      <Team />
+    </>
+  );
 }
