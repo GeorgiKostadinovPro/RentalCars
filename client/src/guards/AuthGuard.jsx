ff599ee08d84ff58fee5e42d51d280c7b564524a
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-03-10 14:57:19 +0200

    Add Authenitcation Guard Component

--- client/src/guards/AuthGuard.jsx
+++ client/src/guards/AuthGuard.jsx
@@ -1,3 +1,14 @@
+import { Navigate, Outlet } from 'react-router-dom'
+
+import { useAuthContext } from '../hooks/useAuthContext'
+import { Path } from '../utilities/Path'
+
 export const AuthGuard = () => {
-    return <></>;
+    const { isUserAuthenticated } = useAuthContext();
+
+    if (!isUserAuthenticated) {
+        return <Navigate to={Path.login} replace={true}/>
+    }
+
+    return <Outlet />;
 }
