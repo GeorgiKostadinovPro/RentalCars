88964820535a7c30517951c107d66857dc0a3618
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-03-15 14:58:51 +0200

    Finish Jest Integration

--- client/src/components/Terms/Terms.test.jsx
+++ client/src/components/Terms/Terms.test.jsx
@@ -1,5 +1,15 @@
+import { render, screen } from '@testing-library/react'
+
 import { Terms } from './Terms'
 
 test("always true test", () => {
     expect(true).toBe.true;
+});
+
+test("heading should be Terms", () => {
+    render(<Terms />);
+
+    const h1 = screen.getByText("Terms");
+
+    expect(h1).toBeInTheDocument();
 });
