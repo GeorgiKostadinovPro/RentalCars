dfb83e7cc7c6cadda650fa07d8790a93d5b59d49
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-03-19 22:28:13 +0200

    Add mocks in ContactForm Tests

--- client/src/components/Contact/ContactForm/ContactForm.test.jsx
+++ client/src/components/Contact/ContactForm/ContactForm.test.jsx
@@ -20,6 +20,8 @@ jest.mock('@emailjs/browser', () => ({
   send: jest.fn(() => Promise.resolve()),
 }));
 
+jest.spyOn(window, 'alert').mockImplementation(() => {});
+
 describe('ContactForm Component', () => {
     let dom;
 
@@ -43,27 +45,27 @@ describe('ContactForm Component', () => {
       expect(headings).toBeInTheDocument();
     });
 
-    // it('submits the form successfully and resets', async () => {
-    //   const nameInput = screen.getByPlaceholderText('Full Name');
-    //   const emailInput = screen.getByPlaceholderText('E-Mail Address');
-    //   const subjectInput = screen.getByPlaceholderText('Subject');
-    //   const messageInput = screen.getByPlaceholderText('Your Message');
-    //   const submitButton = screen.getByText('Send Message');
+    it('submits the form successfully and resets', async () => {
+      const nameInput = screen.getByPlaceholderText('Full Name');
+      const emailInput = screen.getByPlaceholderText('E-Mail Address');
+      const subjectInput = screen.getByPlaceholderText('Subject');
+      const messageInput = screen.getByPlaceholderText('Your Message');
+      const submitButton = screen.getByText('Send Message');
   
-    //   fireEvent.change(nameInput, { target: { value: 'John Doe' } });
-    //   fireEvent.change(emailInput, { target: { value: 'john@example.com' } });
-    //   fireEvent.change(subjectInput, { target: { value: 'Test Subject' } });
-    //   fireEvent.change(messageInput, { target: { value: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.' } });
+      fireEvent.change(nameInput, { target: { value: 'John Doe' } });
+      fireEvent.change(emailInput, { target: { value: 'john@example.com' } });
+      fireEvent.change(subjectInput, { target: { value: 'Test Subject' } });
+      fireEvent.change(messageInput, { target: { value: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.' } });
   
-    //   fireEvent.click(submitButton);
+      fireEvent.click(submitButton);
   
-    //   await waitFor(() => {
-    //     expect(nameInput).toHaveValue('');
-    //     expect(emailInput).toHaveValue('');
-    //     expect(subjectInput).toHaveValue('');
-    //     expect(messageInput).toHaveValue('');
-    //   });
-    // });
+      await waitFor(() => {
+        expect(nameInput).toHaveValue('');
+        expect(emailInput).toHaveValue('');
+        expect(subjectInput).toHaveValue('');
+        expect(messageInput).toHaveValue('');
+      });
+    });
 
     it('validates input fields and shows error messages', async () => {
       const submitButton = screen.getByText('Send Message');
