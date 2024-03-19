50e55dc199c1fa5fd6d73defbf35873cb8020f27
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-03-19 13:56:26 +0200

    Simplify Home Page

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,8 +1,13 @@
+import { Link } from 'react-router-dom'
+
+import { Path } from '../../utilities/Path'
+
 import { Hero } from "./Hero/Hero"
-import { Facts } from "./Facts";
-import { BlogInfo } from "./BlogInfo/BlogInfo";
-import { Services } from "./Services/Services";
-import { RequestContact } from "./RequestContact/RequestContact";
+import { BlogInfo } from "./BlogInfo/BlogInfo"
+import { Services } from "./Services/Services"
+import { RequestContact } from "./RequestContact/RequestContact"
+
+import '../Common/Facts.css'
 
 export const Home = () => {
     return (
@@ -13,7 +18,38 @@ export const Home = () => {
         
         <Services />
 
-        <Facts />
+        <div className="fun-facts">
+          <div className="container">
+            <div className="more-info-content">
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="left-image">
+                    <img
+                      src="assets/images/about-1-570x350.jpg"
+                      className="img-fluid"
+                      alt=""
+                    />
+                  </div>
+                </div>
+                <div className="col-md-6 align-self-center">
+                  <div className="right-content">
+                    <span>Who we are</span>
+                    <h2>
+                      Get to know about <em>our company</em>
+                    </h2>
+                    <p>
+                      Whether you're embarking on a road trip, need a reliable vehicle for a business meeting, 
+                      or simply seeking the freedom to explore a new city, we've got you covered.
+                    </p>
+                    <Link to={Path.about} className="filled-button">
+                      Read More
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
         <BlogInfo />
       </>
