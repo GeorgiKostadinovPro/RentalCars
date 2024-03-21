5560c2c37be0faa486969d1df298563d01e7ec0a
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-03-21 18:50:54 +0200

    Test Review Component

--- client/src/components/Reviews/Review/Review.jsx
+++ client/src/components/Reviews/Review/Review.jsx
@@ -17,7 +17,7 @@ export const Review = ({
               ? author?.profilePictureUrl
               : "https://www.shutterstock.com/image-vector/default-avatar-profile-icon-social-600nw-1677509740.jpg"
           }
-          alt=""
+          alt="User profile"
         />
         <div className="review-content">
           <h5>
