c4591b95cb14910f558f4b3d14229a9a9bf0c0f9
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-04-18 22:04:53 +0300

    Update Menu for Mobile Devices

--- client/src/components/Common/Header/Header.jsx
+++ client/src/components/Common/Header/Header.jsx
@@ -1,6 +1,9 @@
+import { useEffect, useState } from "react"
+
 import { Link } from "react-router-dom"
 
 import NavDropdown from 'react-bootstrap/NavDropdown'
+import Dropdown from 'react-bootstrap/Dropdown'
 
 import { Path } from '../../../utilities/Path'
 import { useAuthContext } from "../../../hooks/useAuthContext"
@@ -11,6 +14,26 @@ import './Header.css'
 
 export const Header = () => {
   const { isUserAuthenticated } = useAuthContext();
+
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setShowMobileMenu(!showMobileMenu);
+  };
+
+  const handleResize = () => {
+    if (window.innerWidth > 992) {
+      setShowMobileMenu(false);
+    }
+  };
+  
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <>
@@ -23,6 +46,7 @@ export const Header = () => {
               <h2>Rental Cars</h2>
             </Link>
             <button
+              onClick={toggleMobileMenu}
               className="navbar-toggler"
               type="button"
               data-toggle="collapse"
@@ -33,60 +57,82 @@ export const Header = () => {
             >
               <span className="navbar-toggler-icon" />
             </button>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link to={Path.home} className="nav-link">
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to={Path.cars} className="nav-link">
-                    Cars
-                  </Link>
-                </li>
-                <NavDropdown
-                  id="nav-dropdown-dark-example"
-                  title="About"
-                  menuVariant="light"
-                >
-                  <NavDropdown.Item as={Link} to={Path.about}>
-                    About us
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to={Path.blog}>
-                    Blog
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to={Path.terms}>
-                    Terms
-                  </NavDropdown.Item>
-                </NavDropdown>
-                <li className="nav-item">
-                  <Link to={Path.contact} className="nav-link">
-                    Contact Us
-                  </Link>
-                </li>
+
+            {showMobileMenu ? (
+              <Dropdown.Menu show style={{ marginTop: "40px", width: "100vw", textAlign: "center" }}>
+                <Dropdown.Item as={Link} to={Path.home}>Home</Dropdown.Item>
+                <Dropdown.Item as={Link} to={Path.cars}>Cars</Dropdown.Item>
+                <Dropdown.Item as={Link} to={Path.about}>About Us</Dropdown.Item>
+                <Dropdown.Item as={Link} to={Path.blog}>Blog</Dropdown.Item>
+                <Dropdown.Item as={Link} to={Path.terms}>Terms</Dropdown.Item>
+                <Dropdown.Item as={Link} to={Path.contact}>Contact Us</Dropdown.Item>
+
+                <Dropdown.Divider />
+
                 {!isUserAuthenticated ? (
+                    <Dropdown.Item as={Link} to={Path.login}>Login</Dropdown.Item>
+                  ) : (
+                    <>
+                      <Dropdown.Item as={Link} to={Path.profile}>Profile</Dropdown.Item>
+                      <Dropdown.Item as={Link} to={Path.logout}>Logout</Dropdown.Item>
+                    </>
+                )}
+              </Dropdown.Menu>
+            ) : (
+              <div className="collapse navbar-collapse">
+                <ul className="navbar-nav ml-auto">
                   <li className="nav-item">
-                    <Link to={Path.login} className="nav-link">
-                      Login
+                    <Link to={Path.home} className="nav-link">
+                      Home
                     </Link>
                   </li>
-                ) : (
-                  <>
+                  <li className="nav-item">
+                    <Link to={Path.cars} className="nav-link">
+                      Cars
+                    </Link>
+                  </li>
+                  <NavDropdown
+                    title="About"
+                    menuVariant="light"
+                  >
+                    <NavDropdown.Item as={Link} to={Path.about}>
+                      About us
+                    </NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to={Path.blog}>
+                      Blog
+                    </NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to={Path.terms}>
+                      Terms
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                  <li className="nav-item">
+                    <Link to={Path.contact} className="nav-link">
+                      Contact Us
+                    </Link>
+                  </li>
+                  {!isUserAuthenticated ? (
                     <li className="nav-item">
-                      <Link to={Path.profile} className="nav-link">
-                        Profile
+                      <Link to={Path.login} className="nav-link">
+                        Login
                       </Link>
                     </li>
-                    <li className="nav-item">
-                      <Link to={Path.logout} className="nav-link">
-                        Logout
-                      </Link>
-                    </li>
-                  </>
-                )}
-              </ul>
-            </div>
+                  ) : (
+                    <>
+                      <li className="nav-item">
+                        <Link to={Path.profile} className="nav-link">
+                          Profile
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link to={Path.logout} className="nav-link">
+                          Logout
+                        </Link>
+                      </li>
+                    </>
+                  )}
+                </ul>
+              </div>
+            )}
           </div>
         </nav>
       </header>
